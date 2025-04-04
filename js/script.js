f6fe11c44e797f2105c39f0c61f83e33fd2ad7b1
Enetwarch <enetwarch@protonmail.com>
2025-04-04 20:24:23 +0800

    reformatted code structure

--- js/script.js
+++ js/script.js
@@ -1,89 +1,82 @@
-const millisecond = 1;
-const second = 1000 * millisecond;
-const minute = 60 * second;
+const MILLISECOND = 1;
+const SECOND = 1000 * MILLISECOND;
+const MINUTE = 60 * SECOND;
 
-window.addEventListener("beforeunload", savePomodoro);
-window.addEventListener("load", () => {
-    addListeners();
-    retrievePomodoro();
-    setInterval(savePomodoro, 1 * minute);
-    displayOutput();
-    changeStateColor("idle");
-});
-
-const listenerConfig = [
-    [() => toggleTimer(), [["click", "play"], ["keyup", "Space"]]],
-    [() => hardReset(), [["touchstart", "reset"], ["mousedown", "reset"]]],
-    [() => softReset(), [["touchend", "reset"], ["mouseup", "reset"]]],
-    [() => changeSettings(), [["click", "settings"]]]
-];
-
-function addListeners() {
-    listenerConfig.forEach(config => {
-        const func = config[0];
-        const listeners = config[1];
-        listeners.forEach(listener => {
-            addListener(listener, func);
-        });
-    }); 
-}
-
-function addListener(listener, func) {
-    const eventType = listener[0];
-    if (eventType !== "keyup") {
-        const id = listener[1];
-        const element = document.getElementById(id);
-        element.addEventListener(eventType, func);
-    } else {
-        const eventCode = listener[1];
-        document.addEventListener(eventType, event => {
-            if (event.code === eventCode) func();
-        });
-    }
-}
-
-let pomodoro;
+let pomodoro = {};
 const defaultPomodoro = {
     "settings": {
-        "workMinutes": 25 * minute,
-        "restMinutes": 5 * minute,
-        "longBreakMinutes": 15 * minute,
+        "workMinutes": 25 * MINUTE,
+        "restMinutes": 5 * MINUTE,
+        "longBreakMinutes": 15 * MINUTE,
         "longBreakInterval": 4,
     },
     "state": "work",
-    "timer": 25 * minute,
+    "timer": 25 * MINUTE,
     "session": 1
 };
 
-function retrievePomodoro() {
+window.addEventListener("load", async () => {  
     const storedPomodoro = localStorage.getItem("pomodoro");
     if (!storedPomodoro) {
         pomodoro = structuredClone(defaultPomodoro);
     } else {
         pomodoro = JSON.parse(storedPomodoro);
     }
-}
 
-function savePomodoro() {
+    setInterval(() => {
+        localStorage.setItem("pomodoro", JSON.stringify(pomodoro));
+    }, 1 * MINUTE);
+    
+    displayPomodoro();
+    changeStateColor("idle");
+
+    if (Notification.permission !== "granted") {
+        await Notification.requestPermission();
+    }
+});
+
+window.addEventListener("beforeunload", () => {
     localStorage.setItem("pomodoro", JSON.stringify(pomodoro));
-}
+});
+
+document.addEventListener("contextmenu", event => event.preventDefault());
+
+const playElement = document.getElementById("play");
+playElement.addEventListener("click", toggleTimer);
+document.addEventListener("keyup", toggleTimer);
+
+const resetElement = document.getElementById("reset");
+resetElement.addEventListener("touchstart", hardReset);
+resetElement.addEventListener("mousedown", hardReset);
+resetElement.addEventListener("touchend", softReset);
+resetElement.addEventListener("mouseup", softReset);
+
+const settingsElement = document.getElementById("settings");
+settingsElement.addEventListener("click", changeSettings);
 
 let isRunning = false;
+let endingTime = null;
 let updateTimerInterval;
 let idleNotificationInterval;
-let endingTime;
 
-function toggleTimer() {
+function toggleTimer(event) {
+    if (event.type === "keyup") {
+        if (event.code !== "Space") return;
+    }
+
     isRunning ? stopTimer() : startTimer();
 }
 
 function stopTimer() {
     isRunning = false;
+    endingTime = null;
     clearInterval(updateTimerInterval);
+
     idleNotificationInterval = setInterval(() => {
         const body = "Timer is idle.";
         pushNotification(body);
-    }, 5 * minute);
+    }, 5 * MINUTE);
+
     changeStateColor("idle");
     changePlayElement();
 }
@@ -91,11 +84,13 @@ function stopTimer() {
 function startTimer() {
     isRunning = true;
     endingTime = Date.now() + pomodoro.timer;
+    clearInterval(idleNotificationInterval);
+
     updateTimerInterval = setInterval(() => {
         pomodoro.timer = endingTime - Date.now();
         pomodoro.timer < 0 ? handleTimerEnd() : displayTimer();    
-    }, 1 * millisecond);
-    clearInterval(idleNotificationInterval);
+    }, 1 * MILLISECOND);
+
     changeStateColor(pomodoro.state);
     changePlayElement();
 }
@@ -110,11 +105,11 @@ const stateColors = {
 function changeStateColor(state) {
     const root = getComputedStyle(document.documentElement);
     const stateColor = root.getPropertyValue(stateColors[state]);
+
     document.body.style.backgroundColor = stateColor;
 }
 
 function changePlayElement() {
-    const playElement = document.getElementById("play");
     if (isRunning) {
         playElement.classList.remove("fa-play");
         playElement.classList.add("fa-pause");
@@ -122,6 +117,7 @@ function changePlayElement() {
         playElement.classList.remove("fa-pause");
         playElement.classList.add("fa-play");
     }
+
     playElement.classList.toggle("inverted");
 }
 
@@ -129,77 +125,92 @@ const stateElement = document.getElementById("state");
 const timerElement = document.getElementById("timer");
 const sessionElement = document.getElementById("session");
 
-function displayOutput() {
+function displayPomodoro() {
     const state = sentenceCase(pomodoro.state);
     const session = `Session ${pomodoro.session}`;
+
     stateElement.innerText = state;
     sessionElement.innerText = session;
+
     displayTimer();
 }
 
 function displayTimer() {
-    let minutes = pomodoro.timer / minute;
-    let seconds = pomodoro.timer % minute / second;
+    let minutes = pomodoro.timer / MINUTE;
+    let seconds = pomodoro.timer % MINUTE / SECOND;
+
     minutes = String(Math.floor(minutes)).padStart(2, "0");
     seconds = String(Math.floor(seconds)).padStart(2, "0");
+
     const time = `${minutes}:${seconds}`;
     timerElement.innerText = time;
 }
 
 function handleTimerEnd() {
-    toggleTimer();
+    if (isRunning) stopTimer();
+
     if (pomodoro.state === "work") {
         const interval = pomodoro.settings.longBreakInterval;
         const notAtInterval = pomodoro.session !== interval;
+
         pomodoro.state = notAtInterval ? "rest" : "longBreak";
     } else {
         const atRestState = pomodoro.state === "rest";
+
         pomodoro.session = atRestState ? pomodoro.session + 1 : 1;
         pomodoro.state = "work";
     }
+
     pomodoro.timer = pomodoro.settings[`${pomodoro.state}Minutes`];
+
     const state = sentenceCase(pomodoro.state);
-    const minutes = pomodoro.timer / minute;
+    const minutes = pomodoro.timer / MINUTE;
     const body = `${state} for ${minutes} minutes.`;
+
     pushNotification(body);
-    displayOutput();
+    displayPomodoro();
 }
 
 async function pushNotification(body) {
     if (Notification.permission !== "granted") {
         await Notification.requestPermission();
     }
+
     if (Notification.permission === "granted") {
         const title = "Pomodoro Timer";
         new Notification(title, { body }); 
     }
 }
 
-const resetElement = document.getElementById("reset");
 let resetButtonPressed = false;
 let hardResetTimeout;
 
 function hardReset() {
     if (resetButtonPressed) return;
     resetButtonPressed = true;
+
     hardResetTimeout = setTimeout(() => {
         if (isRunning) stopTimer();
-        resetElement.classList.toggle("inverted");
+
         setTimeout(() => {
             resetTimer(true);
             resetButtonPressed = false;
-        }, 250 * millisecond);
-    }, 1 * second);
+        }, 250 * MILLISECOND);
+
+        resetElement.classList.toggle("inverted");
+    }, 1 * SECOND);
 }
 
 function softReset() {
-    clearTimeout(hardResetTimeout);
     resetButtonPressed = false;
+    clearTimeout(hardResetTimeout);
     if (isRunning) stopTimer();
-    resetElement.classList.toggle("inverted");
+
     setTimeout(() => {
         resetTimer(false);
-    }, 250 * millisecond);
+    }, 250 * MILLISECOND);
+
+    resetElement.classList.toggle("inverted");
 }
 
 function resetTimer(hardReset) {
@@ -208,45 +219,55 @@ function resetTimer(hardReset) {
             pomodoro.state = "work";
             pomodoro.timer = pomodoro.settings.workMinutes;
             pomodoro.session = 1;
-            displayOutput();
+
+            displayPomodoro();
         }
     } else {
         if (confirm("Do you want to soft reset the timer?")) {
             const stateMinutes = `${pomodoro.state}Minutes`;
             pomodoro.timer = pomodoro.settings[stateMinutes];
+
             displayTimer();    
         }
     }
+
     resetElement.classList.toggle("inverted"); 
 }
 
 function changeSettings() {
     if (isRunning) stopTimer();
-    const settingsElement = document.getElementById("settings");
     settingsElement.classList.toggle("inverted");
+
     setTimeout(() => {
         const settings = structuredClone(pomodoro.settings);
+
         for (const setting in settings) {
             const message = `Modify ${sentenceCase(setting)}`;
             changeSetting(setting, message);
         }
+
         settingsElement.classList.toggle("inverted");
-    }, 250 * millisecond);
+    }, 250 * MILLISECOND);
 }
 
 function changeSetting(setting, message) {
     let defaultValue = pomodoro.settings[setting];
     const isMinutes = setting.includes("Minutes");
-    if (isMinutes) defaultValue /= minute; 
+    if (isMinutes) {
+        defaultValue /= MINUTE;
+    }
+
     while (true) {
         let input = prompt(message, defaultValue);
         if (!input) break;
+
         input = parseInt(input);
         if (isNaN(input)) {
             alert("Please input a number.");
             continue;
         }
-        const changedSetting = isMinutes ? input * minute : input;
+        
+        const changedSetting = isMinutes ? input * MINUTE : input;
         pomodoro.settings[setting] = changedSetting;
         break;
     }
