4c14001832703ed9c690fbf4a94aa959e4efab6c
Enetwarch <enetwarch@protonmail.com>
2025-04-16 22:30:46 +0800

    added a zod form validation system in hard reset prompt

--- src/components/pomodoro-reset.tsx
+++ src/components/pomodoro-reset.tsx
@@ -1,3 +1,9 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
 import { Button } from "@/components/ui/button";
 import { Checkbox } from "@/components/ui/checkbox";
 import {
@@ -10,10 +16,25 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
-import { Label } from "@/components/ui/label";
+import { Form, FormControl, FormField, FormItem, FormLabel } from "@/components/ui/form";
 import { RotateCcw } from "lucide-react";
 
+const formSchema = z.object({
+  neverShowHardResetPromptAgain: z.boolean().default(false).optional(),
+});
+
 export default function PomodoroReset() {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      neverShowHardResetPromptAgain: false,
+    },
+  });
+
+  const onSubmit = (values: z.infer<typeof formSchema>) => {
+    console.log(values);
+  };
+
   return (
     <Dialog>
       <DialogTrigger asChild>
@@ -41,25 +62,37 @@ export default function PomodoroReset() {
               <Button variant="destructive">Hard</Button>
             </DialogTrigger>
             <DialogContent className="w-[300px]">
-              <DialogHeader className="text-left">
-                <DialogTitle>Confirmation</DialogTitle>
-                <DialogDescription>
-                  Are you sure you want to hard reset your timer? This will place your pomodoro state to{" "}
-                  <u>Work Session 1</u>.
-                </DialogDescription>
-              </DialogHeader>
-              <div className="flex flex-col gap-2 py-2">
-                <div className="flex items-center gap-2">
-                  <Checkbox id="neverShowAgain" />
-                  <Label htmlFor="neverShowAgain">Never show this again</Label>
-                </div>
-              </div>
-              <DialogFooter className="flex-row justify-end flex-wrap">
-                <DialogClose asChild>
-                  <Button variant="outline">Cancel</Button>
-                </DialogClose>
-                <Button variant="destructive">Confirm</Button>
-              </DialogFooter>
+              <Form {...form}>
+                <form onSubmit={form.handleSubmit(onSubmit)}>
+                  <DialogHeader className="text-left">
+                    <DialogTitle>Confirmation</DialogTitle>
+                    <DialogDescription>
+                      Are you sure you want to hard reset your timer? This will place your pomodoro state to{" "}
+                      <u>Work Session 1</u>.
+                    </DialogDescription>
+                  </DialogHeader>
+                  <div className="flex py-4">
+                    <FormField
+                      control={form.control}
+                      name="neverShowHardResetPromptAgain"
+                      render={({ field }) => (
+                        <FormItem className="flex items-center gap-2">
+                          <FormControl>
+                            <Checkbox checked={field.value} onCheckedChange={field.onChange} />
+                          </FormControl>
+                          <FormLabel>Never show this again</FormLabel>
+                        </FormItem>
+                      )}
+                    />
+                  </div>
+                  <DialogFooter className="flex-row justify-end flex-wrap">
+                    <DialogClose asChild>
+                      <Button variant="outline">Cancel</Button>
+                    </DialogClose>
+                    <Button variant="destructive">Confirm</Button>
+                  </DialogFooter>
+                </form>
+              </Form>
             </DialogContent>
           </Dialog>
         </DialogFooter>
